a33dfb6ae3ac544909f5eb93176f85ff0df3db77
George Peralta <george.dias@meta.com.br>
2022-09-05 13:33:18 -0300

    config callbacks

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,38 @@
+import { query  as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import GithubProviders from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({  
   providers: [
     GithubProviders({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,      
+      clientSecret: process.env.GITHUB_SECRET,        
     }),    
   ],
+  jwt: {
+    secret: process.env.NEXTAUTH_SECREY,
+  },
+  callbacks: {
+    async signIn({ user, account, profile}) {
+      const { email } = user
+      
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"),
+          {data: { email} }
+          )
+        )        
+        return true
+
+      } catch {
+
+        return false
+
+      }        
+    },
+  },
+  secret: process.env.NEXTAUTH_SECREY,
 })
