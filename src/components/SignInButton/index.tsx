8605efd4a587f81bd3fdb4b544d36b84c3dee392
George Peralta <george.dias@meta.com.br>
2022-09-08 00:57:03 -0300

    removed console log

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from  './styles.module.scss'
 export function SignInButton() {
   const {data: session} = useSession()
 
-  console.log(session)
-
    return session ?  (
       <button type="button" 
       className={styles.signInButton}
@@ -18,13 +16,11 @@ export function SignInButton() {
         &nbsp;{session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
       </button>
-
    ) : (
-
       <button 
       type="button" 
       className={styles.signInButton}
-      onClick={() => signIn("github")}
+      onClick={() => signIn('github')}
       >
         <FaGithub color="#eba417" />
          Sign in with Github
